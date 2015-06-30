374e30f9d3e72655f8028820171d8036f3fc4b6e
chaeplin <rviews@rviews-Air.nemo>
2015-07-01 06:48:17 +0900

    esp-12 with promini
    wifi mqtt door alarm with sleep

---
+++ 01-door-alarm/door-alarm-esp-12/door-alarm-esp-12.ino
@@ -0,0 +1,174 @@
+#include <MQTT.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+#include "ap_setting.h"
+
+
+extern "C"{
+#include "user_interface.h"
+}
+
+extern "C" uint16_t readvdd33(void);
+
+
+int msgsentPin = 16 ; // espRfStatePin
+int doorstatusPin = 5 ; // espDoorPin
+
+int doorstatus ;
+int vdd ;
+
+long startMills ;
+
+char* topic = "esp8266/arduino/s05" ;
+char* hellotopic = "HELLO" ;
+
+// mqtt server
+IPAddress server(192, 168, 10, 10);
+
+String clientName;
+WiFiClient wifiClient;
+
+void callback(const MQTT::Publish& pub) {
+  // handle message arrived
+}
+
+PubSubClient client(wifiClient, server);
+
+void setup()
+{
+  Serial.begin(38400);
+  Serial.println("Starting door alarm"); 
+  pinMode(msgsentPin, OUTPUT);
+  pinMode(doorstatusPin, INPUT);
+
+  digitalWrite(msgsentPin, HIGH);
+  delay(20);
+
+  Serial.println(millis() - startMills);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+ // client.set_callback(callback);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password); 
+  WiFi.config(IPAddress(192, 168, 10, 14), IPAddress(192, 168, 10, 1), IPAddress(255, 255, 255, 0));
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(millis() - startMills);
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  clientName += "esp8266-";
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  clientName += macToStr(mac);
+  clientName += "-";
+  clientName += String(micros() & 0xff, 16);
+
+  doorstatus = digitalRead(doorstatusPin) ;
+  
+  Serial.println(millis() - startMills);
+  
+  Serial.print("doorstatus  : ");
+  Serial.println(doorstatus);
+
+  vdd = readvdd33();
+
+  String payload = "{\"DOOR\":";
+
+  if ( doorstatus == 0 ) {
+          payload += "\"CLOSED\"";
+  } 
+  else {
+          payload += "\"OPEN\"";
+  }
+
+  payload += ",\"vdd\":";
+  payload += vdd;
+  payload += "}";  
+       
+  sendDoorAlarm(payload);
+  
+}
+
+void loop()
+{
+  delay(200);
+  Serial.println(millis() - startMills);
+  Serial.println("going to sleep");
+  ESP.deepSleep(0);
+}
+
+
+void sendDoorAlarm(String payload) {
+
+  if (
+        client.connect(MQTT::Connect((char*) clientName.c_str())
+                .set_clean_session()
+                .set_will("status", "down")
+                .set_keepalive(2))
+    ) {
+    Serial.println("Connected to MQTT broker");
+    Serial.print("Topic is: ");
+    Serial.println(topic);
+
+    if (client.publish(hellotopic, "hello from esp8266/arduino/s05")) {
+      Serial.println("Hello Publish ok");
+    }
+    else {
+      Serial.println("Hello Publish failed");
+    }
+
+  }
+  else {
+    Serial.println("MQTT connect failed");
+    Serial.println("Will reset and try again...");
+    abort();
+  }
+
+
+  if (client.connected()) {
+    Serial.print("Sending payload: ");
+    Serial.println(payload);
+
+    if (client.publish(topic, (char*) payload.c_str())) {
+      Serial.println("Publish ok");
+      Serial.println(millis() - startMills);
+      client.disconnect();
+      Serial.println("set msgsentPin to LOW");
+      digitalWrite(msgsentPin, LOW);
+    }
+    else {
+      Serial.println("Publish failed");
+      abort();
+    }
+  }
+
+}
+
+
+
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+//---------------
+
+
---
+++ 01-door-alarm/door-alarm-pro-mini/door-alarm-pro-mini.ino
@@ -0,0 +1,188 @@
+
+/*
+
+2 - int 0 - door
+
+7 - esp 5
+8 - esp 16
+9 - esp rest
+
+5 - buzzer vcc
+6 - buzzer i/o
+
+*/
+
+
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+int wakeUpPin     = 2;
+
+int espDoorPin    = 7;
+int espRfStatePin = 8;   
+int espResetPin   = 9;
+
+int buzzerVccPin  = 5;
+int buzzerIoPin   = 6;
+
+long startMills;
+
+int espRfstate = HIGH;
+
+int alarmset = LOW ;
+
+volatile int doorStatus ;
+
+void setup()
+{
+  Serial.begin(38400);
+  Serial.println("Starting sleep test");
+  delay(100);  
+  pinMode(wakeUpPin, INPUT_PULLUP);
+
+  doorStatus = digitalRead(wakeUpPin);
+  
+  pinMode(espRfStatePin, INPUT); 
+  pinMode(espResetPin, OUTPUT);
+  pinMode(espDoorPin, OUTPUT);
+
+  pinMode(buzzerVccPin, OUTPUT);
+  pinMode(buzzerIoPin, OUTPUT);
+  
+  attachInterrupt(0, WakeUp, CHANGE);
+  
+  startMills = millis();
+  
+  digitalWrite(espResetPin, LOW);
+  digitalWrite(espDoorPin, doorStatus);
+
+  digitalWrite(buzzerVccPin, LOW);
+  digitalWrite(buzzerIoPin, HIGH);  
+
+  delay(250);
+  sleepNow();
+
+}
+
+void sleepNow()
+{
+  Serial.println("Going sleep");
+  delay(100);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  attachInterrupt(0, WakeUp, CHANGE);
+  
+  sleep_mode();   
+  sleep_disable();
+  Serial.println("Wake up at sleepNow");
+  
+}
+
+void WakeUp()   
+{
+  if ((millis() - startMills) > 300) {
+    
+    Serial.println("Wake up");
+    doorStatus = ! doorStatus ;
+    alarmset = HIGH ;
+    digitalWrite(espDoorPin, doorStatus); 
+    startMills = millis();
+    espReset();  
+  }
+}
+
+void loop()
+{
+  Serial.println(millis() - startMills);
+  
+  espRfstate = digitalRead(espRfStatePin);
+  
+  if (((millis() - startMills) >= 5000) && (espRfstate == HIGH))
+  {
+      alarm_wifi();
+      sleepNow();
+  }
+
+  if (espRfstate == LOW) 
+  {
+       Serial.println("msg sent");
+       sleepNow();
+  }
+
+  if ( alarmset == HIGH ) {
+     if ( doorStatus == HIGH ) 
+     {
+       alarm_open();
+     } else {
+       alarm_close();
+     }
+  }
+     
+  delay(10);
+}
+
+
+void espReset()
+{
+  Serial.println("Reset ESP");
+  digitalWrite(espResetPin, LOW);
+  delay(10);
+  digitalWrite(espResetPin, HIGH);
+}
+
+
+void alarm_wifi()
+{
+  Serial.println("===> wifi has problem");
+  
+  digitalWrite(buzzerVccPin, HIGH);
+  
+  digitalWrite(buzzerIoPin, LOW);
+  delay(300);
+  digitalWrite(buzzerIoPin, HIGH);  
+  delay(100);
+
+  digitalWrite(buzzerIoPin, LOW);
+  delay(300);
+  digitalWrite(buzzerIoPin, HIGH);
+
+  digitalWrite(buzzerVccPin, LOW);
+  
+  alarmset = LOW ;
+  
+}
+
+void alarm_open()
+{
+  Serial.println("===> door opened");
+  
+  digitalWrite(buzzerVccPin, HIGH);
+  
+  digitalWrite(buzzerIoPin, LOW);
+  delay(100);
+  digitalWrite(buzzerIoPin, HIGH);
+  
+  digitalWrite(buzzerVccPin, LOW);  
+  
+  alarmset = LOW ;
+}
+
+void alarm_close()
+{
+  Serial.println("===> door closed");
+  
+  digitalWrite(buzzerVccPin, HIGH);
+  
+  digitalWrite(buzzerIoPin, LOW);
+  delay(80);
+  digitalWrite(buzzerIoPin, HIGH);  
+  delay(100);
+  digitalWrite(buzzerIoPin, LOW);
+  delay(80);
+  digitalWrite(buzzerIoPin, HIGH);
+
+  digitalWrite(buzzerVccPin, LOW);  
+  
+  alarmset = LOW ;
+}
+
