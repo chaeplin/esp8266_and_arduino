7f9ab235495c0c8f365c6da865f13fa53fad3b4b
chaeplin <rviews@rviews-Air.nemo>
2015-07-17 02:53:25 +0900

    low power ir-ac-remote

---
+++ _07-ac-ir-remote-timer/_07-ac-ir-remote-timer.ino
@@ -0,0 +1,41 @@
+#include <LowPower.h>
+// https://github.com/rocketscream/Low-Power
+
+int powerPin = 2;
+
+void setup() {
+  
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, LOW);    
+}
+
+void loop()
+{
+    togglePower();
+    sleep30Minutes();
+    togglePower();
+    sleep60Minutes();
+}
+
+
+void sleep60Minutes()
+{
+  for (int i = 0; i < 450; i++) { 
+     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+  }
+}
+
+
+void sleep30Minutes()
+{
+  for (int i = 0; i < 225; i++) { 
+     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+  }
+}
+
+void togglePower() {
+  digitalWrite(powerPin, HIGH);  
+  delay(100);              
+  digitalWrite(powerPin, LOW);    
+  delay(100);              
+}
