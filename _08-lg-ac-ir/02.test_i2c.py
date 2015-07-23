21227ee07504543c700b6085159b97c37a4b7077
chaeplin <chaeplin@gmail.com>
2015-07-23 15:16:14 +0900

    add example

--- _08-lg-ac-ir/02.test_i2c.py
+++ _08-lg-ac-ir/02.test_i2c.py
@@ -11,3 +11,20 @@ def writeNumber(a, b ):
 
 
 writeNumber(25, 2)
+
+# writeNumber(1, 0)
+# writeNumber(0, 0)
+# writeNumber(2, 1)
+# writeNumber(3, 1)
+# writeNumber(4, 2)
+# writeNumber(5, 26)
+# 
+# 
+# a : mode or temp 		b : air_flow, temp, swing, clean
+# 18 ~ 30 : temp		0 ~ 3 : flow
+# 0 : off				0
+# 1 : on				0
+# 2 : air_swing			0 or 1
+# 3 : air_clean			0 or 1
+# 4 : air_flow			0 ~ 3 : flow
+# 5 : temp				18 ~ 30
