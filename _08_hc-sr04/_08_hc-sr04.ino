e3fe2359c32e13b713fa039b6820fc98955ed636
chaeplin <chaeplin@gmail.com>
2015-07-25 17:32:07 +0900

    adding Average.h

--- _08_hc-sr04/_08_hc-sr04.ino
+++ _08_hc-sr04/_08_hc-sr04.ino
@@ -1,3 +1,5 @@
+#include <Average.h>
+
 #define Trig_pin 4
 
 volatile unsigned long echoBufferx[2];
@@ -6,8 +8,7 @@ volatile unsigned long echoBuffery[2];
 volatile unsigned int x = 0;
 volatile unsigned int y = 0;
 
-int distancex ;
-int distancey ;
+Average<float> ave(5);
 
 void setup() {
   Serial.begin (38400);
@@ -16,9 +17,13 @@ void setup() {
   pinMode(3, INPUT_PULLUP);
 }
 
-void check_distance() {
+long check_distance() {
+  
   x = 0 ;
   y = 0 ;
+
+  int distancex = 0;
+  int distancey = 0;
 
   for ( int i = 0 ; i < 2 ; i++ ) {
     echoBufferx[i] = 0;
@@ -35,38 +40,43 @@ void check_distance() {
   digitalWrite(Trig_pin, HIGH);
   delayMicroseconds(10);
   digitalWrite(Trig_pin, LOW);
-  
-  delay(300);
 
-/*
-  Serial.println( echoBufferx[0] );
-  Serial.println( echoBufferx[1] );
-*/
+  delay(300);
 
   if ( echoBufferx[1] > echoBufferx[0]   ) {
     distancex = ( echoBufferx[1] - echoBufferx[0] ) / 29 / 2 ;
   } else {
-    distancex = 0 ;
+    distancex = 1000 ;
   }
 
   if ( echoBuffery[1] > echoBuffery[0]  ) {
-    distancey = ( echoBuffery[1] - echoBuffery[0] ) / 29 / 2 ;
+     distancey = ( echoBuffery[1] - echoBuffery[0] ) / 29 / 2 ;
   } else {
-    distancey = 0;
+    distancey = 1000;
   }
 
   detachInterrupt(0);
   detachInterrupt(1);
+
+  if ( distancex >= distancey ) {
+    return distancey;
+  } else {
+    return distancex;
+  }
+
 }
 
 void loop() {
 
-  check_distance(); 
-  
-  Serial.print("distance X : ");
-  Serial.print(distancex);
-  Serial.print(" : distance Y : ");
-  Serial.println(distancey);
+  int distance = check_distance();
+  ave.push(distance);
+
+  Serial.print("distance : ");
+  Serial.print(distance);
+  Serial.print(" ave.mean : ");
+  Serial.print(int(ave.mean()));
+  Serial.print(" ave.stddev : ");
+  Serial.println(int(ave.stddev()));
 
   delay(1000); // pause 5 secs
 }
