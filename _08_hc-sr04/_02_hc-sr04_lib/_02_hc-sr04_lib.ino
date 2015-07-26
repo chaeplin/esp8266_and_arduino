d94e6eb706470a5df31b31ea9464738f057ddfe1
chaeplin <chaeplin@gmail.com>
2015-07-26 15:02:56 +0900

    ch change

--- _08_hc-sr04/_02_hc-sr04_lib/_02_hc-sr04_lib.ino
+++ _08_hc-sr04/_02_hc-sr04_lib/_02_hc-sr04_lib.ino
@@ -5,6 +5,7 @@ https://github.com/MajenkoLibraries/Average
 #include <Average.h>
 #include <Ultrasonic.h>
 #include <IRremote.h>
+
 IRsend irsend;
 
 Ultrasonic ultraleft(5,6,30000);   // (Trig PIN,Echo PIN)
@@ -13,6 +14,14 @@ Ultrasonic ultraright(9,10,30000);  // (Trig PIN,Echo PIN)
 Average<float> ave(5);
 
 int power_status = 1 ;
+
+unsigned long  tv_input = 0x20DFD02F;
+unsigned long  tv_right = 0x20DF609F;
+unsigned long  tv_left  = 0x20DFE01F;
+unsigned long  tv_enter = 0x20DF22DD;
+
+int tv_cur_input_no  = 1;
+int tv_move_input_no = 3;
 
 void setup() {
   Serial.begin (38400);
@@ -31,6 +40,26 @@ void setup() {
   pinMode(2, OUTPUT); // GND for IR
   digitalWrite(2, LOW);
   
+}
+
+void tv_input_to_blank(){
+  irsend.sendNEC(tv_input, 32);
+  delay(3000);
+  for ( int i = 0 ; i < ( tv_move_input_no - tv_cur_input_no) ; i++ ) {
+    irsend.sendNEC(tv_right, 32);
+    delay(300);
+  }
+  irsend.sendNEC(tv_enter, 32);
+}
+
+void tv_input_to_cur(){
+  irsend.sendNEC(tv_input, 32);
+  delay(3000);
+  for ( int i = 0 ; i < ( tv_move_input_no - tv_cur_input_no) ; i++ ) {
+    irsend.sendNEC(tv_left, 32);
+    delay(300);
+  }
+  irsend.sendNEC(tv_enter, 32);
 }
 
 long check_distance() {
@@ -59,15 +88,15 @@ void loop() {
   Serial.println(int(ave.stddev()));
 
   if ( ( ave.stddev() < 10) && ( int(ave.mean()) <= 60 )  && ( power_status == 1) ) {
-    Serial.println("tv off");
-    irsend.sendNEC(0x20DF10EF, 32); // LG TV power code
+    Serial.println("to blank ch");
+    tv_input_to_blank();
     delay(50);
     power_status = 0;
   }
 
   if ( ( ave.stddev() < 10) && ( int(ave.mean()) > 100 )  && ( power_status == 0) ) {
-    Serial.println("tv on");
-    irsend.sendNEC(0x20DF10EF, 32); // LG TV power code
+    Serial.println("to cur ch");
+    tv_input_to_cur();
     delay(50);
     power_status = 1;
   }
