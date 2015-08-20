b812eb00c8a904ead669ec890da3f6ac77b807b0
chaeplin <chaeplin@gmail.com>
2015-08-21 07:34:23 +0900

    add timer for light off

--- _02-mqtt-sw/_02-mqtt-sw.ino
+++ _02-mqtt-sw/_02-mqtt-sw.ino
@@ -6,6 +6,9 @@
 #include <ESP8266WiFi.h>
 #include <DallasTemperature.h>
 
+#include <WiFiUdp.h>
+#include <Time.h>
+
 // wifi
 #ifdef __IS_MY_HOME
   #include "/usr/local/src/ap_setting.h"
@@ -38,6 +41,11 @@ DHT dht(DHTPIN, DHTTYPE, 15);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress insideThermometer, outsideThermometer;
+
+//
+unsigned int localPort = 2390;  // local port to listen for UDP packets
+IPAddress timeServer(192, 168, 10, 10); // time.nist.gov NTP server
+const int timeZone = 9;
 
 //
 String clientName;
@@ -89,6 +97,8 @@ void callback(const MQTT::Publish& pub) {
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient, server);
+WiFiUDP udp;
+
 
 void setup() 
 {
@@ -128,6 +138,18 @@ void setup()
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  //
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
+  delay(500);
+  setSyncProvider(getNtpTime);
+
+  if (timeStatus() == timeNotSet) {
+    Serial.println("waiting for sync message");
+  }
 
   attachInterrupt(13, motion_detection, RISING);
   attachInterrupt(5, run_lightcmd, CHANGE); 
@@ -203,6 +225,7 @@ void loop()
 {
   
   changelight();
+  runTimerDoLightOff();
   
   pirValue = digitalRead(pir);
   
@@ -247,6 +270,15 @@ void loop()
   }
 
   client.loop();
+}
+
+void runTimerDoLightOff()
+{
+  if (( relaystatus == 1 ) && ( hour() == 7 ) && ( minute() == 40 ) && ( second() < 5 ))
+  {
+    relaystatus = 0;
+  }
+
 }
 
 void changelight() 
@@ -398,3 +430,62 @@ String macToStr(const uint8_t* mac)
   }
   return result;
 }
+
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  while (udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request called");
+  sendNTPpacket(timeServer);
+  delay(1000);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println(millis() - beginWait);
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress & address)
+{
+  Serial.println("Transmit NTP Request");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+  Serial.println("Transmit NTP Sent");
+}
+//
+//
