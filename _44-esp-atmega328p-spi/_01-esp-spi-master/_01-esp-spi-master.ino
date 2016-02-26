d368739b985b95d24f2823b2fcc7874d306ebedb
chaeplin <chaeplin@gmail.com>
2016-02-27 00:58:34 +0900

    spi test

---
+++ _44-esp-atmega328p-spi/_01-esp-spi-master/_01-esp-spi-master.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+
+typedef struct
+{
+  uint32_t _salt;
+  uint32_t pls;
+  uint16_t ct1;
+  uint16_t ct2;
+  uint16_t ct3;
+  uint16_t pad;
+} data;
+
+data sensor_data;
+
+uint32_t count;
+uint32_t error;
+
+void read_payload(void* buf, uint8_t data_len) {
+  uint8_t* current = reinterpret_cast<uint8_t*>(buf);
+  unsigned int i;
+
+  //SPI.beginTransaction(SPISettings(3000000, MSBFIRST, SPI_MODE0));
+  SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0);
+  delayMicroseconds(15);
+  for (i = 1 ; i <= data_len ; i++) {
+    *current++ = SPI.transfer(i);
+    delayMicroseconds(7);
+  }
+  SPI.endTransaction();
+}
+
+void setup (void)
+{
+  Serial.begin(115200);
+
+  count = error = 0;
+
+  SPI.begin ();
+  SPI.setHwCs(true);
+  Serial.println("");
+}
+
+void loop (void)
+{
+  read_payload(&sensor_data, sizeof(sensor_data));
+  
+  uint16_t test = sensor_data._salt + sensor_data.pls + sensor_data.ct1 + sensor_data.ct2 + sensor_data.ct3;
+
+  if ( sensor_data.pad != test ) {
+    error++;
+  }
+
+  if ( count % 1000 == 0 ) {
+    Serial.print("count : ");
+    Serial.print(count);
+    Serial.print(" - error : ");
+    Serial.println(error);
+  }
+  count++;
+  delay(5);
+}
---
+++ _44-esp-atmega328p-spi/_02-atmega328p-spi-slave/_02-atmega328p-spi-slave.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+
+typedef struct
+{
+  uint32_t _salt;
+  uint32_t pls;
+  uint16_t ct1;
+  uint16_t ct2;
+  uint16_t ct3;
+  uint16_t pad;
+} data;
+
+data sensor_data, sensor_data_copy;
+
+volatile uint8_t* current;
+volatile byte c;
+
+void setup (void)
+{
+  Serial.begin (115200);
+
+  sensor_data._salt = 0;
+  sensor_data.pls   = 10;
+  sensor_data.ct1   = 20;
+  sensor_data.ct2   = 30;
+  sensor_data.ct3   = 40;
+  sensor_data.pad   = sensor_data._salt + sensor_data.pls + sensor_data.ct1 + sensor_data.ct2 + sensor_data.ct3 ;
+
+  sensor_data_copy = sensor_data;
+  
+  SPCR |= bit (SPE);
+  pinMode(MISO, OUTPUT);
+  SPI.attachInterrupt();
+}
+
+ISR (SPI_STC_vect)
+{
+  c = SPDR;
+  if ( c == 0 )
+  {
+    current = reinterpret_cast<uint8_t*>(&sensor_data_copy);
+  }
+  SPDR = *(current + c);
+}
+
+void loop (void)
+{
+  
+  sensor_data._salt++;
+  sensor_data.pls++;
+  sensor_data.ct1++;
+  sensor_data.ct2++;
+  sensor_data.ct3++;
+  sensor_data.pad = sensor_data._salt + sensor_data.pls + sensor_data.ct1 + sensor_data.ct2 + sensor_data.ct3 ;
+
+  if ( c == sizeof(sensor_data) ) {
+    sensor_data_copy = sensor_data;
+    c = sizeof(sensor_data) + 1;
+  }
+  delay(1);
+}
