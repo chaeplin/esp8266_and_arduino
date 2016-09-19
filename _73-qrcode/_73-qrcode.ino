1671745413e4a096182d275de78e8ba31f8451d5
chaeplin <chaeplin@gmail.com>
2016-09-19 14:10:21 +0900

    oled qrcode and nfc with pn532

--- _73-qrcode/_73-qrcode.ino
+++ _73-qrcode/_73-qrcode.ino
@@ -9,11 +9,21 @@
  *   ESP8266 Oled Driver for SSD1306 display by Daniel Eichborn, Fabrice Weinberg
 ***********************************************************************************/
 
+
 #include <qrcode.h>
 #include <SSD1306.h>
 
-SSD1306  display(0x3c, 0, 2);
+SSD1306 display(0x3c, 0, 2);
 QRcode qrcode (&display);
+
+/*
+#include <qrcode.h>
+#include <brzo_i2c.h>
+#include "SSD1306Brzo.h"
+
+SSD1306Brzo display(0x3c, 0, 2);
+QRcode qrcode (&display);
+*/
 
 void setup() {
 
@@ -33,7 +43,7 @@ void setup() {
     qrcode.init();
     // create qrcode
     // https://www.youtube.com/watch?v=DmsfFfD4mQo
-    qrcode.create("dash:XuhHHnjLbm559kzByxtQ6gHMpjs32MHKb2?amount=1");
+    qrcode.create("dash:XxdaVbaPzXm8ow7zZbUeEYftAYpuZhairZ?amount=0.01&instatsend=1");
 
 }
 
---
+++ _74-pn532/_74-pn532.ino
@@ -0,0 +1,74 @@
+// card : elechouse pn532 card
+// lib : https://github.com/elechouse/PN532
+#include "emulatetag.h"
+#include "NdefMessage.h"
+
+
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include "PN532.h"
+
+PN532_SPI pn532spi(SPI, 5);
+EmulateTag nfc(pn532spi);
+
+uint8_t ndefBuf[120];
+NdefMessage message;
+int messageSize;
+
+uint8_t uid[3] = { 0x12, 0x34, 0x56 };
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("------- Emulate Tag --------");
+
+  message = NdefMessage();
+  message.addUriRecord("dash:XxdaVbaPzXm8ow7zZbUeEYftAYpuZhairZ?amount=0.01");
+  messageSize = message.getEncodedSize();
+  if (messageSize > sizeof(ndefBuf)) {
+    Serial.println("ndefBuf is too small");
+    while (1) { }
+  }
+
+  Serial.print("Ndef encoded message size: ");
+  Serial.println(messageSize);
+
+  message.encode(ndefBuf);
+
+  // comment out this command for no ndef message
+  nfc.setNdefFile(ndefBuf, messageSize);
+
+  // uid must be 3 bytes!
+  nfc.setUid(uid);
+
+  nfc.init();
+}
+
+void loop() {
+  // uncomment for overriding ndef in case a write to this tag occured
+  //nfc.setNdefFile(ndefBuf, messageSize);
+
+  // start emulation (blocks)
+  nfc.emulate();
+
+  // or start emulation with timeout
+  /*if(!nfc.emulate(1000)){ // timeout 1 second
+    Serial.println("timed out");
+    }*/
+
+  // deny writing to the tag
+  // nfc.setTagWriteable(false);
+
+  if (nfc.writeOccured()) {
+    Serial.println("\nWrite occured !");
+    uint8_t* tag_buf;
+    uint16_t length;
+
+    nfc.getContent(&tag_buf, &length);
+    NdefMessage msg = NdefMessage(tag_buf, length);
+    msg.print();
+  }
+
+  delay(1000);
+}
+
